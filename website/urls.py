24f28205a2c20341cc94c28d28474012642c073d
Sebastian <sebastian.oliva@unab.cl>
2023-04-16 17:14:57 -0400

    Add register view

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, logout_user
+from .views import home, logout_user, register_user
 
 urlpatterns = [
     path('', home, name='home'),
     # path('login/', login_user, name='login'),
     path('logout_user', logout_user, name='logout'),
+    path('register', register_user, name='register'),
 ]
