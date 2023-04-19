5e1e96a8a06c18754c24bb4dda0c7afceba27531
Sebastian <sebastian.oliva@unab.cl>
2023-04-18 21:55:46 -0400

    Add record view

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, logout_user, register_user
+from .views import home, logout_user, register_user, customer_record
 
 urlpatterns = [
     path('', home, name='home'),
     # path('login/', login_user, name='login'),
     path('logout_user', logout_user, name='logout'),
     path('register', register_user, name='register'),
+    path('records/<int:pk>', customer_record, name='record')
 ]
