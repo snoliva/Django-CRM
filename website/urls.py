44f4bd7c4dc3cf647b0ba1e04231343c89f79950
Sebastian <sebastian.oliva@unab.cl>
2023-04-21 22:03:22 -0400

    Add update record function

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, logout_user, register_user, customer_record, delete_customer, add_record
+from .views import home, logout_user, register_user, customer_record, delete_customer, add_record, update_record
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('records/<int:pk>', customer_record, name='record'),
     path('delete_record/<int:pk>', delete_customer, name='delete'),
     path('add_record', add_record, name='add_record'),
+    path('update_record/<int:pk>', update_record, name='update_record'),
 ]
