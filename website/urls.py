69e5b291ce73b977391f479c39e5c595bc60413d
Sebastian <sebastian.oliva@unab.cl>
2023-04-21 21:29:31 -0400

    Add delete record

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, logout_user, register_user, customer_record
+from .views import home, logout_user, register_user, customer_record, delete_customer
 
 urlpatterns = [
     path('', home, name='home'),
     # path('login/', login_user, name='login'),
     path('logout_user', logout_user, name='logout'),
     path('register', register_user, name='register'),
-    path('records/<int:pk>', customer_record, name='record')
+    path('records/<int:pk>', customer_record, name='record'),
+    path('delete_record/<int:pk>', delete_customer, name='delete'),
 ]
--- website/views.py
+++ website/views.py
@@ -54,3 +54,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in home page ...")
         return redirect('home')
+    
+
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Records deleted Successfully/ ...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in home page ...")
+        return redirect('home')
